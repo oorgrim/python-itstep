3d3bcca07b09738a186971b51913215261daafb2
oorgrim <99543878+oorgrim@users.noreply.github.com>
2023-11-24 21:45:51 +0600

    (то, что успела сделать в шаге)

--- Nov-24/00.py
+++ Nov-24/00.py
@@ -1,24 +1,24 @@
 user_data = input("Введите числа от 0 до 9: ")
 match user_data:
-    case 0:
+    case "0":
         print(")")
-    case 1:
+    case "1":
         print("!")
-    case 2:
+    case "2":
         print("@")
-    case 3:
+    case "3":
         print("#")
-    case 4:
+    case "4":
         print("$")
-    case 5:
+    case "5":
         print("%")
-    case 6:
+    case "6":
         print("^")
-    case 7:
+    case "7":
         print("&")
-    case 8:
+    case "8":
         print("*")
-    case 9:
+    case "9":
         print("(")
     case _:
         print("На данной кнопке нет числа")
