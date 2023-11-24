8d0da3f4e7221d5760d2883e899962f8ce807a6a
oorgrim <99543878+oorgrim@users.noreply.github.com>
2023-11-24 21:38:45 +0600

    Create 00.py

---
+++ Nov-24/00.py
@@ -0,0 +1,24 @@
+user_data = input("Введите числа от 0 до 9: ")
+match user_data:
+    case 0:
+        print(")")
+    case 1:
+        print("!")
+    case 2:
+        print("@")
+    case 3:
+        print("#")
+    case 4:
+        print("$")
+    case 5:
+        print("%")
+    case 6:
+        print("^")
+    case 7:
+        print("&")
+    case 8:
+        print("*")
+    case 9:
+        print("(")
+    case _:
+        print("На данной кнопке нет числа")
