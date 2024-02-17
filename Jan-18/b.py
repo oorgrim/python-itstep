dc6fcab55eba1746aae1d140c0418baf58febd35
oorgrim <oorgrim@gmail.com>
2024-02-17 18:19:34 +0600

    Jan-18 venv + pygame

--- Jan-18/b.py
+++ Jan-18/b.py
@@ -3,10 +3,10 @@ import pygame as pg #импортруем скачанный пайгейм ко
 
 pg.init()
 screen = pg.display.set_mode((1024, 768)) # кортеж из x и y (кортеж из двух элементов)
-screen.fill(('white'))
-pg.draw.rect(screen, (255, 0, 0), (100, 100, 50, 50))
-pg.draw.rect(screen, ('pink'), (200, 100, 50, 50))
-pg.draw.circle(screen, ('aqua'), (400, 120), 30)
+screen.fill(('white')) #заполнить экран белым цвтом
+pg.draw.rect(screen, (255, 0, 0), (100, 100, 50, 50)) #нарисовать прямоуг
+pg.draw.rect(screen, ('pink'), (200, 100, 50, 50)) #розовый прямоугольник
+pg.draw.circle(screen, ('aqua'), (400, 120), 30) # нарисовать круг
 # цикл игры
 
 gameover = False
@@ -15,5 +15,5 @@ while not gameover:
     for e in events:
         if e.type == pg.QUIT:
             gameover = True
-    pg.display.update()
-pg.quit()
+    pg.display.update() #Обновить экран (чтобы были видны изменения и тд)
+pg.quit() #выход
