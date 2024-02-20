491648bd73adee2c456e1552aabfc0baa209a872
oorgrim <oorgrim@gmail.com>
2024-02-20 22:17:09 +0600

    UPD оказывается ДЗ за 25 января подписала под 2 февраля

---
+++ Dec-28/00.py
@@ -0,0 +1,18 @@
+def get_count_of_emoji(count_emoji):
+    emoji = "😊"
+    return  emoji * count_emoji
+
+try:
+    user_input = input('Сколько вы хотите смайликов? ')
+    count_emoji = int(user_input)
+    result = get_count_of_emoji(count_emoji)
+    print(result)
+
+except ValueError:
+    print("Ведите целое число")
+
+def test_get_count_of_emoji():
+    assert result.count("😊") == count_emoji
+    print("Тест пройден")
+
+test_get_count_of_emoji()
---
+++ Feb-8/money.py
@@ -0,0 +1,3 @@
+class Money:
+    def __init__(self, how_much, currency) -> None:
+        pass
